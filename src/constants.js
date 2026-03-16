47718170a9ce7f5af76657763fdbebb37d0f2a51
Pablo Nicolas  Segato <pablo.segato47@gmail.com>
2026-03-16 19:05:00 +0100

    gestion de proyectos mediante una constante, donde puedo cargar futuros proyectos sin estar cargandolos con html

---
+++ src/constants.js
@@ -0,0 +1,30 @@
+export const proyectos = [
+  {
+    title: "La Casa de Olga",
+    link: "https://github.com/PabloSegato/La_casa_de_Olga",
+    description:
+      "Sitio web para mostrar una casa de alquiler. Presentación de la propiedad, galería e información de contacto.",
+    web: "https://github.com/PabloSegato/La_casa_de_Olga",
+    tec:["React ", "JavaScript ", "CSS "]
+  },
+  {
+    title: "App Weather",
+    link: "https://github.com/PabloSegato/AppWheater",
+    description:
+      "Aplicación del clima con búsqueda por ciudad e integración de API meteorológica externa.",
+    web: "https://appweather-pnsdeveloper.netlify.app",
+  },
+  {
+    title: "Films Search",
+    link: "https://github.com/PabloSegato/Films_Web",
+    description: "Buscador de películas con detalles y valoraciones.",
+    web: "https://films-search-pnsdeveloper.netlify.app",
+  },
+  {
+    title: "IMC Calculator",
+    link: "https://github.com/PabloSegato/IMC-Calculate",
+    description:
+      "Calculadora de IMC con validación de inputs y clasificación del resultado por categoría.",
+    web: "https://imc-pnsdeveloper.netlify.app",
+  },
+];
--- src/section/PerfilProfesional.jsx
+++ src/section/PerfilProfesional.jsx
@@ -1,4 +1,5 @@
 import "./PerfilProfesional.css";
+import { proyectos } from "../constants.js";
 
 export const PerfilProfesional = () => {
   return (
@@ -22,11 +23,11 @@ export const PerfilProfesional = () => {
         <div className="container-encabezado-proyectos">
           <h3>
             <strong>Tech Developer — Bootcamp Intensivo</strong>
-          </h3>{" "}
+          </h3>
           <p> 2025 — 2026 </p>
         </div>
 
-        <div className="container-proyectos">
+        <div className="container-estudios">
           <h3> Digital House</h3>
           <ul>
             <li>
@@ -44,6 +45,31 @@ export const PerfilProfesional = () => {
           </ul>
         </div>
       </section>
+
+      <section>
+        <h2>PROYECTOS</h2>
+        {proyectos.map((item) => (
+          <div className="card-proyectos" key={item.title}>
+            <h3>{item.title}</h3>
+            <a href={item.link} target="_blank">
+              {item.title} GitHub
+            </a>
+            <span className="tec">{item.tec}</span>
+
+            <p>{item.description}</p>
+            <a href={item.web} className="boton-demo">
+              Demo en vivo
+            </a>
+          </div>
+        ))}
+      </section>
     </div>
   );
 };
+
+// Quede en la secuencia de cargar las tecnologias por cada uno
+// terminar de dar formato a lo que falta de los proyectos
+// Para finalizar con las competencias
+// posteriormente vamos a realiar el acomodado de github
+//  con los proyectos y tambien actualziarlos para  darle una mejor
+// vista y organizacion de todo
